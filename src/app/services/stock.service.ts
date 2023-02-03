0ac6ff2f517ad134efc1ab63998c398e2f4dcaf4
Denny Wibowo <indocoffee@gmail.com>
2023-02-03 16:12:55 -0500

    create stock service based on json files

---
+++ src/app/services/stock.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import aapl from '../mocks/aapl.json';
+import ino from '../mocks/ino.json';
+import pltr from '../mocks/pltr.json';
+import pypl from '../mocks/pypl.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StockService {
+  stocksHardcoded = [aapl, ino, pltr, pypl];
+  stockMap = new Map();
+  constructor() {
+    this.translateStock();
+  }
+
+  translateStock() {
+    this.stocksHardcoded.forEach((stock) => {
+      let editedData: any[] = [];
+      const monthlyTimeSeries: any = stock['Monthly Time Series'];
+      Object.keys(monthlyTimeSeries).forEach((key) => {
+        editedData.push({
+          x: new Date(key),
+          y: parseFloat(monthlyTimeSeries[key]['4. close']),
+        });
+      });
+
+      this.stockMap.set(stock['Meta Data']['2. Symbol'], editedData);
+    });
+  }
+
+  getRandomStock() {
+    const max = this.stocksHardcoded.length - 1;
+    const min = 0;
+    const random = Math.floor(Math.random() * (max - min + 1) + min);
+    const symbol = this.stocksHardcoded[random]['Meta Data']['2. Symbol'];
+    return { symbol, data: this.stockMap.get(symbol) };
+  }
+}
+/*
+
+*/
