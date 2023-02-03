faa30bcef6dad29c6c879233ddd1d2602200218c
Denny Wibowo <indocoffee@gmail.com>
2023-02-03 16:13:38 -0500

    Create graph component

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -1,23 +1,47 @@
-import { Component } from '@angular/core';
-import Highcharts from 'highcharts/es-modules/masters/highcharts.src';
+import { Component, Input } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.scss'],
 })
 export class GraphComponent {
-  Highcharts = Highcharts;
-  linechart: any = {
+  @Input()
+  stocks: any[] = [];
+
+  Highcharts: typeof Highcharts = Highcharts;
+  updateFlag = false;
+
+  linechart: Highcharts.Options = {
     series: [
       {
-        data: [1, 2, 3],
+        type: 'line',
+        data: this.stocks[0]?.data,
       },
     ],
+    xAxis: {
+      type: 'datetime',
+    },
     chart: {
       type: 'line',
     },
     title: {
-      text: 'linechart',
+      text: '',
     },
   };
+
+  updateGraph() {}
+
+  ngOnInit() {
+    if (this.linechart.series?.length) {
+      this.linechart.series[0] = {
+        type: 'line',
+        name: this.stocks[0].symbol,
+        data: this.stocks[0].data,
+      };
+    }
+
+    this.updateFlag = true;
+  }
 }
