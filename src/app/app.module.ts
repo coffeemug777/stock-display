4d02b68c7e7ce569cdd739f962cc83d7567b3bb6
Denny Wibowo <indocoffee@gmail.com>
2023-02-03 20:15:36 -0500

    Remove highcharts and add chartjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GraphComponent } from './components/graph/graph.component';
-import { HighchartsChartModule } from 'highcharts-angular';
 import { ChangeCompareTickerComponent } from './components/change-compare-ticker/change-compare-ticker.component';
 
 @NgModule({
@@ -19,13 +18,7 @@ import { ChangeCompareTickerComponent } from './components/change-compare-ticker
     GraphComponent,
     ChangeCompareTickerComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    FormsModule,
-    HighchartsChartModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
