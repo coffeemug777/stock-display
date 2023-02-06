8ec8dc81d357a8ebe158cf6e58be88727aae0dd3
Denny Wibowo <indocoffee@gmail.com>
2023-02-06 00:18:54 -0500

    fix unit testing

--- src/app/resolvers/new-ticker.resolver.spec.ts
+++ src/app/resolvers/new-ticker.resolver.spec.ts
@@ -1,14 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { StockService } from '../services/stock.service';
 
 import { NewTickerResolver } from './new-ticker.resolver';
 
 describe('NewTickerResolver', () => {
   let resolver: NewTickerResolver;
+  let stockService: StockService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [StockService],
+    });
     resolver = TestBed.inject(NewTickerResolver);
+    stockService = TestBed.inject(StockService);
   });
 
   it('should be created', () => {
